e2b7914baf8a9fbfba88bc9913fd0af7363a8736
alesiods <alesioadrover@gmail.com>
2026-01-16 09:05:50 -0300

    Agregado de asyncMock, ItemList e Item

--- src/componentes/ItemListContainer/ItemListContainer.jsx
+++ src/componentes/ItemListContainer/ItemListContainer.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import { useState, useEffect } from "react"
+import { getProductos } from "../../asyncmock"
+import ItemList from "../ItemList/ItemList"
 
 const ItemListContainer = () => {
+
+    const [productos, setProductos] = useState([])
+
+    useEffect(()=>{
+      getProductos()
+        .then(respuesta => setProductos(respuesta))
+        .catch(error => console.log(error))
+    },[])
+
   return (
-    <div>ItemListContainer</div>
+    <>
+      <h2 style={{textAlign:"center"}}>Mis Productos</h2>
+      <ItemList productos={productos}/>
+    </>
   )
 }
 
